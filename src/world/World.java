40bec1598d439ed97d3fdf45f3b5858e36f635b5
Joel Manning <stopwatchj@gmail.com>
2016-11-24 11:40:25 -0800

    Sets Galore!

--- src/world/World.java
+++ src/world/World.java
@@ -1,40 +1,37 @@
 package world;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class World {
 	
-	private List<Planet> planets;
-	private List<Ship> ships;
-	private List<Ship> species;
+	private final Set<Planet> planets;
+	private final Set<Ship> ships;
+	private final Set<Species> species;
 	public World() {
-		
+		planets = new HashSet<Planet>();
+		ships = new HashSet<Ship>();
+		species = new HashSet<Species>();
 	}
 	/**
 	 * @return the planets
 	 */
-	public List<Planet> getPlanets() {
+	public Set<Planet> getPlanets() {
 		return planets;
-	}
-	/**
-	 * @param planets the planets to set
-	 */
-	public void setPlanets(ArrayList<Planet> planets) {
-		this.planets = planets;
 	}
 	/**
 	 * @return the ships
 	 */
-	public List<Ship> getShips() {
+	public Set<Ship> getShips() {
 		return ships;
 	}
 	/**
-	 * @param ships the ships to set
+	 * @return the species
 	 */
-	public void setShips(ArrayList<Ship> ships) {
-		this.ships = ships;
+	public Set<Species> getSpecies() {
+		return species;
 	}
-	
 	
 }
