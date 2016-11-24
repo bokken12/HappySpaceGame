b0e03fc31b9915ede7613e8bbab166aceb91739d
Joel Manning <stopwatchj@gmail.com>
2016-11-24 09:52:20 -0800

    UpperCamelCase

--- src/networking/NotAMessageException.java
+++ src/networking/NotAMessageException.java
@@ -7,14 +7,14 @@ package networking;
  * @author joelmanning
  *
  */
-public class notAMessageException extends Exception {
+public class NotAMessageException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1466867333228673259L;
 	
-	public notAMessageException(){
+	public NotAMessageException(){
 		super("Recieved a class other than Message in the listener.");
 	}
 }
---
+++ src/server/Handler.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package server;
+
+import java.net.Socket;
+
+/**
+ * @author joelmanning
+ *
+ */
+public class Handler {
+	public Handler(Socket s){
+		
+	}
+}
